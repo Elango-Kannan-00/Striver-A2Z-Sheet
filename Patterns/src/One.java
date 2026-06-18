77c3535eacd723ec0a380c913d0e82f861323665
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-18 23:53:26 +0530

    Added square pattern with star.

---
+++ Patterns/src/One.java
@@ -0,0 +1,29 @@
+/*
+
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+ * * * * *
+
+ */
+
+import java.util.Scanner;
+
+public class One {
+    public static void main (String [] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter the range: ");
+        int n = scan.nextInt();
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
