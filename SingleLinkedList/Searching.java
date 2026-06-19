3dd774c227a4223108e33346f99973d2d69e1e90
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-20 00:01:43 +0530

    Added searching in a linnked list.

---
+++ SingleLinkedList/Searching.java
@@ -0,0 +1,56 @@
+package SingleLinkedList;
+
+import java.util.Scanner;
+
+// class Node {
+//     int data;
+//     Node next;
+
+//     public Node(int data) {
+//         this.data = data;
+//     }
+// }
+
+class LinkedList extends InsertionOperations {
+
+    // Searching an element in a singly linked list.
+    public void searching(int data) {
+        Node current = head;
+        
+        boolean isFound = false;
+        int result = 0;
+        while (current.next != null) {
+            if (current.data != data) {
+                current = current.next;
+            } else {
+                isFound = true;
+                result = current.data;
+                break;
+            }
+        }
+
+        if (isFound) {
+            System.out.println(result);
+        } else {
+            System.out.println("No such element found!");
+        }
+    }
+}
+
+public class Searching extends InsertionOperations {
+    public static void main (String[] args) {
+
+        LinkedList obj = new LinkedList();
+
+        obj.insertAtEnd(10);
+        obj.insertAtEnd(20);
+        obj.insertAtEnd(30);
+        obj.insertAtEnd(40);
+        obj.insertAtEnd(50);
+        obj.insertAtEnd(60);
+
+        obj.searching(30);
+        obj.searching(100);
+
+    }
+}
