b751ca12c30530fca8d3b659d0301c6a17f9c33f
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-21 23:11:12 +0530

    Added finding loop detection in singly linked list file.

---
+++ SingleLinkedList/DetectLoop.java
@@ -0,0 +1,63 @@
+package SingleLinkedList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class LoopFinding extends InsertionOperations {
+
+    // Time complexity - O(N), Extra space (Hashset) - O(1)
+    public boolean detectLoop() {
+        Set<Node> set = new HashSet<>();
+
+        Node current = head;
+
+        while (current != null) {
+            if (set.contains(current)) {
+                return true;
+            }
+
+            set.add(current);
+            current = current.next;
+        }
+        return false;        
+    }
+
+    // Another method to find the loop in LL. 
+    // Time complexity - O(N), Space complexity - O(1);
+
+    // public boolean detectLoop() {
+    //     Node slow = head;
+    //     Node fast = head;
+
+    //     while (slow.next != null && fast.next != null) {
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+
+    //         if (slow == fast) {
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
+    //}
+}
+
+public class DetectLoop {
+    public static void main (String[] args) {
+
+        LoopFinding obj = new LoopFinding();
+        
+        obj.insertAtEnd(10);
+        obj.insertAtEnd(20);
+        obj.insertAtEnd(30);
+        obj.insertAtEnd(40);
+        obj.insertAtEnd(50);
+        obj.insertAtEnd(60);
+        obj.insertAtEnd(70);
+
+        boolean result = obj.detectLoop();
+        if (result) System.out.println("Loop detected!");
+        else System.out.println("No Loop detected!");
+        
+    }
+}
