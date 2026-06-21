3e4afcf6681da7865a8aed9d166111ce4ec57eb6
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-22 00:28:04 +0530

    Added length of the digits file.

--- Know_Basic_Maths/Count_Digits.java
+++ Know_Basic_Maths/Count_Digits.java
@@ -1,5 +1,36 @@
 package Know_Basic_Maths;
 
+import java.util.Scanner;
+
+/*
+
+Example 1:
+Input:N = 12345
+Output:5
+Explanation:  The number 12345 has 5 digits.
+                        
+Example 2:
+Input:N = 7789              
+Output: 4
+Explanation: The number 7789 has 4 digits.  
+
+*/
+
 public class Count_Digits {
-    
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter any number: ");
+        int num = scan.nextInt();
+
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num /= 10;
+        }
+
+        System.out.println(count);
+        
+        scan.close();   
+    }
 }
