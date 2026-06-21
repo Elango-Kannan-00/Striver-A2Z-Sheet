f8f04499da86dc6bf70e4a9f585b2bce8dc1b48b
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-06-22 00:21:15 +0530

    Added file.

--- Patterns/Six.java
+++ Patterns/Six.java
@@ -19,7 +19,7 @@ public class Six {
         System.out.println("Enter the range: ");
         int n = scan.nextInt();
 
-        for (int i=5; i>=1; i--) {
+        for (int i=n; i>=1; i--) {
             for (int j=1; j<=i; j++) {
                 System.out.print(j + " ");
             }
