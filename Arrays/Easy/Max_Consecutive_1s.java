9158cddab8d256e7b46d9b3bd42eafa4c1561e1d
Elango-Kannan-00 <elango.kiot@gmail.com>
2026-07-09 22:27:18 +0530

    Added file.

--- Arrays/Easy/Max_Consecutive_1s.java
+++ Arrays/Easy/Max_Consecutive_1s.java
@@ -1,5 +1,20 @@
 package Arrays.Easy;
 
+/*
+
+Example 1:
+Input: prices = {1, 1, 0, 1, 1, 1}
+Output: 3
+Explanation: There are two consecutive 1’s and three consecutive 1’s in the array out of which maximum is 3.
+
+Example 2:
+Input: prices = {1, 0, 1, 1, 0, 1} 
+Output: 2
+Explanation: There are two consecutive 1's in the array. 
+            
+
+*/
+
 import java.util.Scanner;
 
 public class Max_Consecutive_1s {
